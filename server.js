912fad186bc704fe4be7bc6c055fb1517a44916d
Madhumida <smadhumida@gmail.com>
2022-04-12 11:15:38 -0700

    Adding public folder and partials

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const pokemon = require("./models/pokemon");
 const app = express();
 
+const pokemons = require("./models/pokemon.js")
+const morgan = require("morgan");
+const methodOverride = require("method-override");
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+//Logging
+app.use(morgan("tiny"));
+//Public Folder Files
+app.use("/static", express.static("public"));
+//Overriding Methods for PUT and DELETE
+app.use(methodOverride("_method"));
 
 app.get("/", (req, res) => {
     res.send("Server is working")
+})
+
+app.get("/pokemon", (req, res) => {
+    res.render("index.ejs", { allPokemon: pokemons })
 })
 
 app.listen("3000", () => {
